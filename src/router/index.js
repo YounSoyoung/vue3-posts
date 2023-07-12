e98c27d618a4ca7c177b7382ff8f6e56439b3529
YounSoyoung <grace902853@gmail.com>
2023-07-12 11:05:28 +0900

    페이지 라우터 끝

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
+import PostCreateView from '@/views/posts/PostCreateView.vue';
+import PostDetailView from '@/views/posts/PostDetailView.vue';
+import PostListView from '@/views/posts/PostListView.vue';
+import PostEditView from '@/views/posts/PostEditView.vue';
 
 const routes = [
 	{
@@ -12,6 +16,28 @@ const routes = [
 		path: '/about',
 		name: 'About',
 		component: AboutView,
+	},
+	{
+		path: '/posts',
+		name: 'PostList',
+		component: PostListView,
+	},
+	{
+		path: '/posts/create',
+		name: 'PostCreate',
+		component: PostCreateView,
+	},
+	{
+		//동적 라우팅
+		//:id - 파라미터, 라우터 객체에 params으로 받을 수 있다, id가 파라미터명
+		path: '/posts/:id',
+		name: 'PostDetail',
+		component: PostDetailView,
+	},
+	{
+		path: '/posts/:id/edit',
+		name: 'PostEdit',
+		component: PostEditView,
 	},
 ];
 
