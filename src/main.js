dd90dad1611bad76b4e9516cb0dcf5dbb0600c82
YounSoyoung <grace902853@gmail.com>
2023-07-20 21:59:37 +0900

    store 기본 구조

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import globalDirectives from './plugins/global-directives';
 import dayjs from './plugins/dayjs';
-// import focus from './directives/focus';
-// import globalComponents from '@/plugins/global-components';
 
 const app = createApp(App);
 // app.use(globalComponents);
@@ -16,6 +15,7 @@ const app = createApp(App);
 app.use(router);
 app.use(globalDirectives);
 app.use(dayjs);
+app.use(createPinia());
 app.mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
---
+++ src/stores/counter.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia';
+
+//defineStore(id값, store에 관련된 옵션)
+//store도 컴포저블 함수처럼 use로 시작하는 이름을 짓는 것이 관례이다
+//'counter' - store의 고유한 아이디, 뷰 dev tools와 연결하는데 사용된다.
+export const useCounterStore = defineStore('counter', {
+	state: () => ({
+		counter: 0,
+	}),
+	getters: {
+		doubleCount: state => state.counter * 2,
+	},
+	actions: {
+		increment() {
+			this.counter++;
+		},
+	},
+});
